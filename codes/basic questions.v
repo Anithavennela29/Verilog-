8d455171d37d50fb493e65a7915fa015da03d9e7
Anithavennela29 <115879884+Anithavennela29@users.noreply.github.com>
2025-06-22 20:34:10 +0530

    basic questions.v

--- codes/basic questions.v
+++ codes/basic questions.v
@@ -126,3 +126,75 @@ rsr_dut;
 #200 $finish;
 end
 endmodule
+
+//om=nly 40Mhz 40 %duty cycle
+module clk_divider(clk_40_40,src_clk,count_posedge,
+reset);
+input src_clk, reset;
+output clk_40_40;
+output reg[2:0]count_posedge;
+reg a1,a2,a3;
+always@(posedge src_clk)
+begin
+if(reset)
+count_posedge <= 0;
+else
+begin
+if(count_posedge == 4)
+count_posedge <= 0;
+else
+count_posedge <= count_posedge + 1;
+end
+end
+always@(posedge src_clk)
+begin
+if(count_posedge == 0)
+a1 <= 1;
+else
+a1 <= 0;
+end
+always@(negedge src_clk)
+begin
+if(count_posedge == 3)
+a2 <= 1;
+else
+a2 <= 0;
+end
+
+
+assign clk_40_40 = a1 | a2;
+
+endmodule
+
+// Code your testbench here
+// or browse Examples
+
+`timescale 1ns/1ns
+module test;
+reg src_clk, reset;
+  wire clk_40_40;
+  wire [2:0]count_posedge;
+clk_divider DUT(clk_40_40,src_clk,count_posedge,
+reset);
+initial
+begin
+src_clk = 0;
+forever
+#5 src_clk = ~src_clk;
+end
+task rsr_dut;
+begin
+@(negedge src_clk)
+reset = 1;
+@(negedge src_clk)
+reset = 0;
+end
+endtask
+initial
+begin
+  $dumpfile("dump.vcd");
+    $dumpvars(1);
+rsr_dut;
+#200 $finish;
+end
+endmodule
