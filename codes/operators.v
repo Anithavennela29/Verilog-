c6ffe4e925af1d0cf4460d0924715cd61addef39
Anithavennela29 <115879884+Anithavennela29@users.noreply.github.com>
2025-07-18 12:19:03 +0530

    operators.v

--- codes/operators.v
+++ codes/operators.v
@@ -36,6 +36,8 @@ module top;
 endmodule
 
 y=111
+
+
 module top;
   reg [2:0] a,b;
   reg [2:0]y;
@@ -75,3 +77,49 @@ module top;
 endmodule
 
 y=010
+
+module top;
+reg [3:0]a=4'b0100;
+reg [3:0]b;
+initial
+begin
+b=a+1'bx;
+$monitor("b=%b",b);
+end
+endmodule
+b=xxxx
+
+
+module top;
+reg [3:0]a=4'b010x;
+wire y=(a==4'b1100)?1'b1:1'b0;
+initial
+begin
+//b=a+1'bx;
+$monitor("y=%b",y);
+end
+endmodule
+
+y=0;
+
+module top;
+reg [3:0]a=4'b110x;
+wire y=(a==4'b1100)?1'b1:1'b0;
+initial
+begin
+//b=a+1'bx;
+$monitor("y=%b",y);
+end
+endmodule
+y=x
+
+module top;
+reg [3:0]a=4'b110x;
+wire y=(a===4'b1100)?1'b1:1'b0;
+initial
+begin
+//b=a+1'bx;
+$monitor("y=%b",y);
+end
+endmodule
+y=0
